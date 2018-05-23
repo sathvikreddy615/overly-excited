7760dd0ff79b590365496d4f8de87796e7e75864
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-23 12:06:00 -0500

    finished all excercises for practicing functions

--- overly-excited.js
+++ overly-excited.js
@@ -12,22 +12,65 @@
 
 // addExcitement(sentence);
 
+// let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+// const addExcitement = theWordArray => {
+//     let buildUp = "";
+//     let bang = "!";
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         if ((i + 1) % 3 === 0) {
+//             console.log(buildUp += `${theWordArray[i]} ${bang} `);
+//             bang += "!";
+            
+//         } else {
+//             console.log(buildUp += `${theWordArray[i]} `);
+//         }
+//     }
+// }
+
+// addExcitement(sentence);
+
+// let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+// const addExcitement = (theWordArray, symbol) => {
+//     let buildUp = "";
+//     let insertSymbol = symbol;
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         if ((i + 1) % 3 === 0) {
+//             console.log(buildUp += `${theWordArray[i]} ${insertSymbol} `);
+//             insertSymbol += symbol;  
+//         } else {
+//             console.log(buildUp += `${theWordArray[i]} `);
+//         }
+//     }
+// }
+
+// addExcitement(sentence, "?");
+
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-const addExcitement = theWordArray => {
+let sentence2 = ["The", "person", "is", "cool"];
+
+const addExcitement = (theWordArray, symbol) => {
     let buildUp = "";
-    let bang = "!";
+    let insertSymbol = symbol;
     for (let i = 0; i < theWordArray.length; i++) {
         if ((i + 1) % 3 === 0) {
-            console.log(buildUp += `${theWordArray[i]} ${bang} `);
-            bang += bang;
-            
+            console.log(buildUp += `${theWordArray[i]} ${insertSymbol} `);
+            insertSymbol += symbol;  
         } else {
             console.log(buildUp += `${theWordArray[i]} `);
         }
     }
 }
 
-addExcitement(sentence);
+addExcitement(sentence, "?");
+addExcitement(sentence2, "$");
+
+
+
+
+
+
 
 
