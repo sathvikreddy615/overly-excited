31e9b3f523c92d6743047ae96216f0b55e2abba9
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-23 10:47:49 -0500

    Completed first overly-excited challenge, moving onto the next part

---
+++ overly-excited.js
@@ -0,0 +1,27 @@
+// Overly Excited Challanged
+
+// let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+// const addExcitement = theWordArray => {
+//     let buildUp = "";
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         buildUp += `${theWordArray[i]} `;
+//         console.log(buildUp);
+//     }
+// }
+
+// addExcitement(sentence);
+
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+const addExcitement = theWordArray => {
+    let buildUp = "";
+    for (let i = 0; i < theWordArray.length; i++) {
+        buildUp += `${theWordArray[i]} `;
+        console.log(buildUp);
+    }
+}
+
+addExcitement(sentence);
+
+
